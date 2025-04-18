725e74149b0cf6f0ddaf67ac7780d99392e6f4e0
Haader <37Hader38@gmail.com>
2025-04-18 01:40:29 -0300

    hiden

--- googleUser.js
+++ googleUser.js
@@ -42,6 +42,7 @@ function obtenerIdUsuario() {
         <div style="font-size: 1.2rem; font-weight: bold;">${name}</div>
       </div>
     `;
+    document.getElementById("containerApp").style.display = "flex";
   } else {
     document.getElementById("profile-info").innerHTML += `
       <div id="google-signin-btn" style="text-align: center; padding: 20px;">
@@ -52,6 +53,7 @@ function obtenerIdUsuario() {
         <button onclick="iniciarLogin()" style="padding: 10px 20px; font-size: 1rem;">Acceder</button>
       </div>
     `;
+    document.getElementById("containerApp").style.display = "hiden";
   }
 }
 
