acb1ae01ad903cd8d9c3066c1037537e27172309
Haader <37Hader38@gmail.com>
2025-04-17 23:59:40 -0300

    user

--- googleUser.js
+++ googleUser.js
@@ -27,3 +27,30 @@ window.addEventListener("message", (event) => {
   localStorage.setItem("token", token);
   localStorage.setItem("user", JSON.stringify(profile));
 });
+
+function obtenerIdUsuario() {
+  let user_google = localStorage.getItem("user");
+
+  if (user_google) {
+    let user_google_json = JSON.parse(user_google);
+    const { name, email, picture } = user_google_json;
+
+    document.getElementById("profile-info").innerHTML = `
+      <div style="display: flex; flex-direction: column; align-items: center; padding: 20px;">
+        <img src="${picture}" alt="Foto de perfil" style="width: 100px; height: 100px; border-radius: 50%; box-shadow: 0 4px 8px rgba(0,0,0,0.2); margin-bottom: 10px;">
+        <div style="font-size: 1.2rem; font-weight: bold;">${name}</div>
+        <div style="font-size: 0.9rem; color: gray;">${email}</div>
+      </div>
+    `;
+  } else {
+    document.getElementById("profile-info").innerHTML = `
+      <div id="google-signin-btn" style="text-align: center; padding: 20px;">
+      <img src="https://www.svgrepo.com/show/382106/user-circle.svg" 
+     alt="Usuario no identificado" 
+     style="width: 100px; height: 100px; border-radius: 50%; box-shadow: 0 4px 8px rgba(0,0,0,0.2); margin-bottom: 10px; background-color: #f0f0f0; padding: 10px;">
+
+        <button onclick="iniciarLogin()" style="padding: 10px 20px; font-size: 1rem;">Acceder</button>
+      </div>
+    `;
+  }
+}
