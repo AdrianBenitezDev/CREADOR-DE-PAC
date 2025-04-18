2e333180c7581f41e470b586875b2ea989fc4ffa
Haader <37Hader38@gmail.com>
2025-04-18 00:09:23 -0300

    asdw

--- googleUser.js
+++ googleUser.js
@@ -33,13 +33,12 @@ function obtenerIdUsuario() {
 
   if (user_google) {
     let user_google_json = JSON.parse(user_google);
-    const { name, email, picture } = user_google_json;
+    const { name, picture } = user_google_json;
 
     document.getElementById("profile-info").innerHTML = `
       <div style="display: flex; flex-direction: column; align-items: center; padding: 20px;">
         <img src="${picture}" alt="Foto de perfil" style="width: 100px; height: 100px; border-radius: 50%; box-shadow: 0 4px 8px rgba(0,0,0,0.2); margin-bottom: 10px;">
         <div style="font-size: 1.2rem; font-weight: bold;">${name}</div>
-        <div style="font-size: 0.9rem; color: gray;">${email}</div>
       </div>
     `;
   } else {
